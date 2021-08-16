ad4a0d1cbfe4537385557d8d7a2f196331914d4d
ERIK GONZALEZ GARCÍA <erik-gabriel.gonzalez-garcia@capgemini.com>
2021-08-16 16:15:18 +0200

    fixed download button logic

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,10 +1,10 @@
-import AppToolbar from "./AppToolbar"
-import { EDITOR_TOOLBAR } from "../shared/constants"
-import { useDispatch, useSelector } from "react-redux"
-import { Divider, Grid, makeStyles } from "@material-ui/core"
-import { TwitterPicker } from "react-color"
-import { useEffect, useState } from "react"
-import axios from "axios"
+import AppToolbar from './AppToolbar'
+import { EDITOR_TOOLBAR } from '../shared/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { Divider, Grid, makeStyles } from '@material-ui/core'
+import { TwitterPicker } from 'react-color'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import {
   addKey,
   connectedAction,
@@ -13,16 +13,16 @@ import {
   setIsHost,
   setHaveMap,
   setSocket,
-} from "../redux/slices/AppSlice"
-import { io } from "socket.io-client"
-import toast, { Toaster } from "react-hot-toast"
+} from '../redux/slices/AppSlice'
+import { io } from 'socket.io-client'
+import toast, { Toaster } from 'react-hot-toast'
 import {
   LOGIN_SUCCESSFULL,
   CONNECT_SUCCESSFULL,
   DISCONNECT_SUCCESSFULL,
-} from "../utils/literals.js"
+} from '../utils/literals.js'
 
-import { SHARE_DRAW_CONFIG } from "../shared/socket-actions"
+import { SHARE_DRAW_CONFIG } from '../shared/socket-actions'
 
 let prevPos = { offsetX: 0, offsetY: 0 }
 let line = []
@@ -34,56 +34,56 @@ let socket = null
 
 const useStyles = makeStyles((theme) => ({
   mapContainer: {
-    display: "flex",
-    position: "relative",
+    display: 'flex',
+    position: 'relative',
   },
 
   canvas: {
-    border: "1px solid #000000",
+    border: '1px solid #000000',
     left: 0,
-    position: "absolute",
+    position: 'absolute',
     top: 0,
   },
   colorsContainer: {
-    display: "flex",
-    flexDirection: "column",
+    display: 'flex',
+    flexDirection: 'column',
   },
   colorsOptionsContainer: {
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
   },
   colorsSavesContainer: {
-    textAlign: "center",
-    display: "flex",
-    padding: "2rem 9px",
+    textAlign: 'center',
+    display: 'flex',
+    padding: '2rem 9px',
   },
   colorsSavesButtonClass: {
-    height: "30px",
-    width: "30px",
-    cursor: "pointer",
-    position: "relative",
-    outline: "none",
-    float: "left",
-    borderRadius: "4px",
-    borderWidth: "0",
-    margin: "0px 6px 6px 0px",
+    height: '30px',
+    width: '30px',
+    cursor: 'pointer',
+    position: 'relative',
+    outline: 'none',
+    float: 'left',
+    borderRadius: '4px',
+    borderWidth: '0',
+    margin: '0px 6px 6px 0px',
   },
   colorsTitleClass: {
-    textAlign: "center",
+    textAlign: 'center',
   },
   drawToolbarContainer: {
-    display: "flex",
-    flexDirection: "row",
-    height: "7rem",
-    backgroundColor: "#cfd1e3",
+    display: 'flex',
+    flexDirection: 'row',
+    height: '7rem',
+    backgroundColor: '#cfd1e3',
     border: `1px solid ${theme.palette.divider}`,
     borderRadius: theme.shape.borderRadius,
   },
   shapesContainer: {
-    display: "flex",
-    flexDirection: "column",
-    marginLeft: "2rem",
-    marginRight: "2rem",
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: '2rem',
+    marginRight: '2rem',
   },
 }))
 
@@ -98,19 +98,19 @@ const Main = () => {
   const isHost = useSelector((res) => res.state.isHost)
   // const mapFile = useSelector((res) => res.state.img)
 
-  const [mapFile, setMapFile] = useState("")
+  const [mapFile, setMapFile] = useState('')
   const [ctx, setCtx] = useState(null)
   const [isPainting, setIsPainting] = useState(false)
   const [nextColor, setNextColor] = useState(0)
   const [drawingFigure, setDrawingFigure] = useState(0)
   const [drawConfig, setDrawConfig] = useState({
-    lineJoin: "round",
+    lineJoin: 'round',
     lineWidth: 2,
-    strokeStyle: "black",
+    strokeStyle: 'black',
   })
 
   useEffect(() => {
-    const ctxAux = canvas.getContext("2d")
+    const ctxAux = canvas.getContext('2d')
     // ctxAux.strokeStyle = drawConfig.strokeStyle
     // ctxAux.lineJoin = drawConfig.lineJoin
     // ctxAux.lineWidth = drawConfig.lineWidth
@@ -227,7 +227,7 @@ const Main = () => {
   }
 
   const emitDrawing = (prevPos, currPos, room) => {
-    socket.emit("broadcast drawing", {
+    socket.emit('broadcast drawing', {
       prevPos: prevPos,
       currPos: currPos,
       room,
@@ -274,7 +274,7 @@ const Main = () => {
       dispatch(updateInRoom(false))
     } else {
       if (user) {
-        socket = io("http://localhost:4000", {
+        socket = io('http://localhost:4000', {
           reconnectionDelayMax: 10000,
           reconnectionAttempts: 5,
         })
@@ -282,7 +282,7 @@ const Main = () => {
         // setSocket(socket)
         console.log(socket)
 
-        socket.on("connected", () => {
+        socket.on('connected', () => {
           axios
             .post(`http://localhost:4000/api/users`, {
               name: user.name,
@@ -298,31 +298,31 @@ const Main = () => {
             })
         })
 
-        socket.on("disconnect", () => {
+        socket.on('disconnect', () => {
           toast.success(DISCONNECT_SUCCESSFULL)
           dispatch(disconnectedAction())
         })
 
-        socket.on("broadcast res", (res) => {
-          console.log("broadcast res", res)
+        socket.on('broadcast res', (res) => {
+          console.log('broadcast res', res)
         })
 
-        socket.on("receiving image", (res) => {
+        socket.on('receiving image', (res) => {
           dispatch(setHaveMap(true))
           setMapFile(res.image)
           // setHaveMap(res)
         })
 
-        socket.on("receiving drawing", (res) => {
+        socket.on('receiving drawing', (res) => {
           draw(res.prevPos, res.currPos)
         })
 
-        socket.on("user joined", () => {
+        socket.on('user joined', () => {
           dispatch(setIsHost(true))
           dispatch(updateInRoom(true))
           if (haveMap) {
             socket.emit(
-              "broadcast image",
+              'broadcast image',
               mapFile,
               roomKey !== null ? roomKey : socket.id
             )
@@ -332,24 +332,24 @@ const Main = () => {
             drawConfig,
             roomKey !== null ? roomKey : socket.id
           )
-          toast("User joined!", {
-            icon: "🙋‍♀️",
+          toast('User joined!', {
+            icon: '🙋‍♀️',
           })
         })
 
-        socket.on("joined", (targetId) => {
+        socket.on('joined', (targetId) => {
           dispatch(addKey(targetId))
-          toast.success("Joined!")
+          toast.success('Joined!')
         })
 
-        socket.on("already joined", () => {
-          toast("Already joined!", {
-            icon: "⚠️",
+        socket.on('already joined', () => {
+          toast('Already joined!', {
+            icon: '⚠️',
           })
         })
 
         socket.on(SHARE_DRAW_CONFIG, (config) => {
-          console.log("RECEIVE: SHARE_DRAW_CONFIG", config)
+          console.log('RECEIVE: SHARE_DRAW_CONFIG', config)
           setDrawConfig(config)
           setCanvasContextConfig(config)
         })
@@ -427,19 +427,20 @@ const Main = () => {
    * Handle download button. Combine both canvas and image to get the final image.
    */
   const download = () => {
-    // save canvas draw
-    const canvasDraw = canvas.toDataURL()
-    const imgDraw = new Image()
-    imgDraw.src = canvasDraw
-    // combine both canvas
-    ctx.drawImage(image, 0, 0)
-    ctx.globalAlpha = 0.5
-    ctx.drawImage(imgDraw, 0, 0)
-    const compositedImg = canvas.toDataURL()
-    // download
-    const link = document.createElement("a")
-    link.download = "map_modified.png"
-    link.href = compositedImg
+    // create new canvas ankd set properties
+    const canvasAux = document.createElement('canvas')
+    canvasAux.width = canvas.width
+    canvasAux.height = canvas.height
+
+    // get new canvas context and draw first the img, then the old canvas draw
+    const ctxAux = canvasAux.getContext('2d')
+    ctxAux.drawImage(image, 0, 0)
+    ctxAux.drawImage(canvas, 0, 0)
+
+    // create and download img
+    const link = document.createElement('a')
+    link.download = 'map_modified.png'
+    link.href = canvasAux.toDataURL()
     link.click()
   }
 
@@ -449,7 +450,7 @@ const Main = () => {
 
   return (
     <div>
-      <Toaster position="top-center" reverseOrder={false} />
+      <Toaster position='top-center' reverseOrder={false} />
       <AppToolbar
         type={EDITOR_TOOLBAR}
         connect={connect}
@@ -461,7 +462,7 @@ const Main = () => {
       {haveMap && (
         <Grid
           container
-          alignItems="center"
+          alignItems='center'
           className={classes.drawToolbarContainer}
         >
           <div className={classes.shapesContainer}>
@@ -470,7 +471,7 @@ const Main = () => {
             <button onClick={() => setDrawingFigure(2)}>Rectangle</button>
             <button onClick={() => setDrawingFigure(3)}>Circle</button>
           </div>
-          <Divider orientation="vertical" flexItem />
+          <Divider orientation='vertical' flexItem />
           <div className={classes.shapesContainer}>
             Size
             <button onClick={() => setLineWidth(2)}>Line 1</button>
@@ -478,10 +479,10 @@ const Main = () => {
             <button onClick={() => setLineWidth(6)}>Line 3</button>
             <button onClick={() => setLineWidth(8)}>Line 4</button>
           </div>
-          <Divider orientation="vertical" flexItem />
+          <Divider orientation='vertical' flexItem />
           <div className={classes.colorsContainer}>
             <div className={classes.colorsOptionsContainer}>
-              <TwitterPicker triangle="hide" onChangeComplete={saveColor} />
+              <TwitterPicker triangle='hide' onChangeComplete={saveColor} />
               <div className={classes.colorsSavesContainer}>
                 <button
                   className={classes.colorsSavesButtonClass}
@@ -525,17 +526,17 @@ const Main = () => {
           </div>
         </Grid>
       )}
-      <div className={classes.mapContainer} id="map-container">
+      <div className={classes.mapContainer} id='map-container'>
         <img
           ref={(ref) => (image = ref)}
           hidden={!haveMap}
           src={mapFile}
-          alt="map"
+          alt='map'
           onLoad={handleImageLoaded}
-          id="mapImage"
+          id='mapImage'
         />
         <canvas
-          id="mapCanvas"
+          id='mapCanvas'
           hidden={!haveMap}
           className={classes.canvas}
           ref={(ref) => (canvas = ref)}
