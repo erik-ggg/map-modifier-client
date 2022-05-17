d9e928493085d5867a41c356b73e203dec08a5fd
erik-ggg <gabriel.balbona.otero@gmail.com>
2022-05-17 16:07:35 +0200

    Removed unused code

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -16,8 +16,6 @@ import toast from 'react-hot-toast'
 import {
   BROADCAST_DRAWING,
   CONNECT,
-  CONNECTED,
-  // BROADCAST_IMAGE,
   END_DRAWING,
   RECEIVING_DRAWING,
   RECEIVING_IMAGE,
@@ -63,16 +61,10 @@ const Main = ({ socket }) => {
   const inRoom = useSelector((res) => res.state.inRoom)
   const haveMap = useSelector((res) => res.state.haveMap)
   const user = useSelector((res) => res.state.user)
-  const isConnected = useSelector((res) => res.state.isConnected)
-  // const isHost = useSelector((res) => res.state.isHost)
-  // const mapFile = useSelector((res) => res.state.img)
-  // const [imageLoaded, setImageLoaded] = useState(false)
   const [mapFile, setMapFile] = useState(null)
   const [ctx, setCtx] = useState(null)
   const [isPainting, setIsPainting] = useState(false)
   const [drawingFigure, setDrawingFigure] = useState(0)
-  // const [isErasing, setIsErasing] = useState(false)
-  // const [haveMapAux, setHaveMapAux] = useState(null)
   const [drawConfig, setDrawConfig] = useState({
     lineJoin: 'round',
     lineWidth: 2,
